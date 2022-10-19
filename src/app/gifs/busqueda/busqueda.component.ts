b7e83e0f58afacce5a35ee0e6dbc33b930cb7be8
nalbarracin <naza16@gmail.com>
2022-10-19 15:41:57 -0300

    Inicio historial de busquedas

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -9,11 +10,14 @@ export class BusquedaComponent {
   @ViewChild('txtBuscar')
   txtBuscar!: ElementRef<HTMLInputElement>;
 
+  constructor( private gifsService:GifsService ) {}
+
   buscar() {
     const valor = this.txtBuscar.nativeElement.value;
     console.log(valor)
 
     this.txtBuscar.nativeElement.value = '';
+    this.gifsService.buscarGifs( valor );
   }
 
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+
+    constructor( private gifsService:GifsService) {}
+  
+    get historial(): string[] {
+      return this.gifsService.historial;
+    } 
+
+}
